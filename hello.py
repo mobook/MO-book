a941cc87c4f4cfddd300687476a2f9f8187f6c8f
Jeffrey Kantor <kantor.1@nd.edu>
2022-03-14 12:29:55 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("Hello, World")
+import subprocess
+import sys
+
+def install(package):
+    subprocess.check_call(sys.executable, "-m", "pip", "install", package)
+    
+    
+if __name__ == "__main__":
+    install("pyomo")
