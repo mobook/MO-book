29d71380a46d6684a1e7bfdab5d4ca6e9615281b
Jeffrey Kantor <kantor.1@nd.edu>
2022-03-14 12:47:24 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     pip_install("gurobipy")
     pip_install("cplex")
     pip_install("xpress")
-    subprocess(["wget", "-N", "-q", "https://ampl.com/dl/open/ipopt/ipopt-linux64.zip"])
+    subprocess(["wget", "-N", "-q", '"https://ampl.com/dl/open/ipopt/ipopt-linux64.zip"'])
     subprocess(["unzip", "-o", "-q", "ipopt-linux64"])
     subprocess(["apt-get", "install", "-y", "-q", "coinor-cbc"])
     
