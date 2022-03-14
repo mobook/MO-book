5f9c35f5c36fe1dacb21db6b6b0f416d5291d400
Jeffrey Kantor <kantor.1@nd.edu>
2022-03-14 12:34:47 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ import subprocess
 import sys
 
 def install(package):
-    subprocess.run("pip", "install", package)
+    subprocess.run(["pip", "install", package])
     
     
 if __name__ == "__main__":
