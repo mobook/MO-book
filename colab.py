88eaf56b9a7c78aa69612d12ad8b6a15f4c27725
Jeffrey Kantor <kantor.1@nd.edu>
2022-03-15 10:46:20 -0400

    Update colab.py

--- colab.py
+++ colab.py
@@ -29,45 +29,6 @@ def ampl_install(package):
     else:
         print(f"success")  
         
-import pyomo.environ as pyo
-
-def test_model():
-    m = pyo.ConcreteModel()
-    m.x = pyo.Var(bounds=(0, 10))
-    m.y = pyo.Var(bounds=(0, 10))
-
-    @m.Objective(sense=pyo.maximize)
-    def obj(m):
-        return m.x + m.y
-
-    @m.Constraint()
-    def con(m):
-        return m.x + 2*m.y <= 5
-
-    return m
-
-def test_solver(solver):
-    m = test_model()
-    print(f'testing {solver} ...', end='')
-    try:
-        pyo.SolverFactory(solver).solve(m)
-        print('success')
-    except:
-        return
-
-test_solver('gurobi_direct')
-test_solver('cplex')
-test_solver('xpress')
-
-test_solver('glpk')
-test_solver('cbc')
-
-test_solver('ipopt')
-test_solver('bonmin')
-test_solver('couenne')
-test_solver('jacop')
-test_solver('gecode')
-
 if "google.colab" in sys.modules:
 
     # install from pipy
@@ -86,9 +47,10 @@ if "google.colab" in sys.modules:
     ampl_install('couenne')
     ampl_install('gecode')
     ampl_install('jacop')
+
+import pyomo.environ as pyo
     
-    
-    def test_model():
+def test_model():
     m = pyo.ConcreteModel()
     m.x = pyo.Var(bounds=(0, 10))
     m.y = pyo.Var(bounds=(0, 10))
