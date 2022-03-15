d66881359dbec688b8e62b9e36b8de2254b6393e
Jeffrey Kantor <Kantor.1@nd.edu>
2022-03-14 21:21:57 -0400

    Update colab.py

--- colab.py
+++ colab.py
@@ -1,13 +1,14 @@
 import shutil
 import sys
 import os.path
+import subprocess
 
 if not shutil.which("pyomo"):
     if "google.colab" in sys.modules:
-        !pip install -q pyomo
-        !pip install -q gurobipy
-        !pip install -q cplex
-        !pip install -q xpress
-        !apt-get install -y -qq coinor-cbc
-        !wget -N -q "https://ampl.com/dl/open/ipopt/ipopt-linux64.zip"
-        !unzip -o -q ipopt-linux64
+        subprocess.run(["pip", "install", "-q", "pyomo"])
+        #!pip install -q gurobipy
+        #!pip install -q cplex
+        #!pip install -q xpress
+        #!apt-get install -y -qq coinor-cbc
+        #!wget -N -q "https://ampl.com/dl/open/ipopt/ipopt-linux64.zip"
+        #!unzip -o -q ipopt-linux64
