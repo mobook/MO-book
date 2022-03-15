4f93db5be00346e0c50559c4c44e9a63e56128d3
Jeffrey Kantor <kantor.1@nd.edu>
2022-03-15 10:37:28 -0400

    Update colab.py

--- colab.py
+++ colab.py
@@ -1,43 +1,50 @@
 import sys
-<<<<<<< HEAD
 import subprocess
 
 def pip_install(package):
-    print(f"installing {package} ... ", end="")
+    print(f"installing (pip) {package} ... ", end="")
     out = subprocess.run(['pip3', 'install', '-q', package])
     if out.returncode:
         print(f"failed")
     else:
         print(f"success")
 
+def apt_install(package):
+    print(f"installing (apt) {package} ... ", end="")
+    out = subprocess.run(['apt-get', 'install', '-y', '-q', package])
+    if out.returncode:
+        print(f"failed")
+    else:
+        print(f"success") 
+
+def ampl_install(package):
+    print(f"installing (ampl) {package} ... ", end="")    
+    out = subprocess.run(['wget', '-N', '-q', f'https://ampl.com/dl/open/{package}/{package}-linux64.zip'])  
+    if out.returncode:
+        print(f"failed to download")
+        return
+    out = subprocess.run(['unzip', '-o', '-q', f'{package}-linux64'])
+    if out.returncode:
+        print(f"failed to unzip")
+    else:
+        print(f"success")  
+
 if "google.colab" in sys.modules:
+
+    # install from pipy
     pip_install('pyomo')
     pip_install('gurobipy')
     pip_install('cplex')
     pip_install('xpress')
+
+    # install debian packages
+    apt_install('glpk-utils')
+    apt_install('coinor-cbc')
+
+    # install ampl binaries
+    ampl_install('ipopt')
+    ampl_install('bonmin')
+    ampl_install('couenne')
+    ampl_install('gecode')
+    ampl_install('jacop')
     
-        
-        
-#import shutil
-#import sys
-#import os.path
-#import subprocess
-#import google
-
-#if "google.colab" in sys.modules:
-#    subprocess.run(["pip", "install", "-q", "pyomo"])
- #   subprocess.run(["pip", "install", "-q", "gurobipy"])
-=======
-import os.path
-import subprocess
-
-if not shutil.which("pyomo"):
-    if "google.colab" in sys.modules:
-        subprocess.run(["pip", "install", "-q", "pyomo"])
->>>>>>> 1bb124cb14e7e53aacc8e660be054059abdbc291
-        #!pip install -q gurobipy
-        #!pip install -q cplex
-        #!pip install -q xpress
-        #!apt-get install -y -qq coinor-cbc
-        #!wget -N -q "https://ampl.com/dl/open/ipopt/ipopt-linux64.zip"
-        #!unzip -o -q ipopt-linux64
