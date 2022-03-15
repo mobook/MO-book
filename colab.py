f06ceae4f41f4fdd36810871f77a35991390ddcf
Jeffrey Kantor <Kantor.1@nd.edu>
2022-03-15 11:30:53 -0400

    Update colab.py

--- colab.py
+++ colab.py
@@ -1,24 +1,27 @@
 import sys
 import subprocess
 
-def pip_install(package):
-    print(f"installing (pip) {package} ... ", end="")
+def pip_install(package, solver=None):
+    print(f"{package} ...", end="")
+    print(f"installing ...", end="")
     out = subprocess.run(['pip3', 'install', '-q', package])
     if out.returncode:
         print(f"failed")
-    else:
-        print(f"success")
+        return
+    if solver is not None:
+        test_solver(solver)
 
-def apt_install(package):
-    print(f"installing (apt) {package} ... ", end="")
+def apt_install(package, solver=None):
+    print(f"installing {package} ... ", end="")
     out = subprocess.run(['apt-get', 'install', '-y', '-q', package])
     if out.returncode:
         print(f"failed")
-    else:
-        print(f"success") 
+        return
+    if solver is not None:
+        test_solver(solver)
 
-def ampl_install(package):
-    print(f"installing (ampl) {package} ... ", end="")    
+def ampl_install(package, solver=None):
+    print(f"installing {package} ... ", end="")    
     out = subprocess.run(['wget', '-N', '-q', f'https://ampl.com/dl/open/{package}/{package}-linux64.zip'])  
     if out.returncode:
         print(f"failed to download")
@@ -26,30 +29,10 @@ def ampl_install(package):
     out = subprocess.run(['unzip', '-o', '-q', f'{package}-linux64'])
     if out.returncode:
         print(f"failed to unzip")
-    else:
-        print(f"success")  
+        return
+    if solver is not None:
+        test_solver(solver) 
         
-if "google.colab" in sys.modules:
-
-    # install from pipy
-    pip_install('pyomo')
-    pip_install('gurobipy')
-    pip_install('cplex')
-    pip_install('xpress')
-
-    # install debian packages
-    apt_install('glpk-utils')
-    apt_install('coinor-cbc')
-
-    # install ampl binaries
-    ampl_install('ipopt')
-    ampl_install('bonmin')
-    ampl_install('couenne')
-    ampl_install('gecode')
-    ampl_install('jacop')
-
-import pyomo.environ as pyo
-    
 def test_model():
     m = pyo.ConcreteModel()
     m.x = pyo.Var(bounds=(0, 10))
@@ -67,22 +50,53 @@ def test_model():
 
 def test_solver(solver):
     m = test_model()
-    print(f'testing {solver} ...', end='')
+    print(f"testing {solver} ...", end="")
     try:
         pyo.SolverFactory(solver).solve(m)
         print('success')
     except:
         return
+        
+if "google.colab" in sys.modules:
 
-test_solver('gurobi_direct')
-test_solver('cplex')
-test_solver('xpress')
+    # install Pyomo
+    pip_install('pyomo')
+    try:
+        import pyomo.environ as pyo
+        print()
+    except:
+        print("pyomo not found")
+        quit()
+    
+    # install solvers from PiPy
+    pip_install('gurobipy', 'gurobi_direct')
+    #pip_install('cplex', 'cplex')
+    pip_install('xpress', 'xpress')
 
-test_solver('glpk')
-test_solver('cbc')
+    # install debian packages
+    apt_install('glpk-utils', 'glpk')
+    apt_install('coinor-cbc', 'cbc')
 
-test_solver('ipopt')
-test_solver('bonmin')
-test_solver('couenne')
-test_solver('jacop')
-test_solver('gecode')
+    # install ampl binaries
+    ampl_install('ipopt', 'ipopt')
+    ampl_install('bonmin', 'bonmin')
+    ampl_install('couenne', 'couenne')
+    #ampl_install('gecode', 'gecode')
+    #ampl_install('jacop', 'jacop')
+
+import pyomo.environ as pyo
+    
+def test_model():
+    m = pyo.ConcreteModel()
+    m.x = pyo.Var(bounds=(0, 10))
+    m.y = pyo.Var(bounds=(0, 10))
+
+    @m.Objective(sense=pyo.maximize)
+    def obj(m):
+        return m.x + m.y
+
+    @m.Constraint()
+    def con(m):
+        return m.x + 2*m.y <= 5
+
+    return m
