8edd2f103a17b7880b3965facd3ae138f418bad0
Jeffrey Kantor <kantor.1@nd.edu>
2022-03-18 13:57:22 -0400

    Update install_on_colab.py

--- tools/install_on_colab.py
+++ tools/install_on_colab.py
@@ -8,33 +8,57 @@ async def run(cmd: str):
         stderr=asyncio.subprocess.PIPE
         )
     stdout, stderr = await proc.communicate()
-    #print(f"{cmd} exited with {proc.returncode}")
-    #if stdout:
-    #    print(stdout.decode())
     if stderr:
         print(stderr.decode())
     return proc.returncode
 
-async def pip(pkg:str, solver:str):
-    if not await run(f'pip3 install -q {pkg}'):
-        test_solver(solver)
-    else:
-        print(f". {solver} not installed")
+async def pip_install(pkg:str, solver:str, test=lp_test):
+    if await run(f'pip3 install -q {pkg}'):
+        print(f"{solver} not installed . ", ) 
+        return
+    test(solver)
+    return
 
-async def apt(pkg:str, solver:str):
-    if not await run(f'apt-get install -y -q {pkg}'):
-        test_solver(solver)
-    else:
+async def apt_install(pkg:str, solver:str, test=lp_test):
+    if await run(f'apt-get install -y -q {pkg}'):
         print(f"{solver} not installed . ")
+        return
+    test(solver)
+    return
 
-async def ampl(pkg:str, solver:str):
-    await run(f'wget -N -q https://ampl.com/dl/open/{pkg}/{pkg}-linux64.zip')
-    if not await run(f'unzip -o -q {pkg}-linux64'):
-        test_solver(solver)
-    else:
-        print(f"{solver} failed download . ")
+async def ampl_install(pkg:str, solver:str, test=lp_test):
+    if await run(f'wget -N -q https://ampl.com/dl/open/{pkg}/{pkg}-linux64.zip'):
+        print(f"{pkg} failed to download . ")
+        return
+    if await run(f'unzip -o -q {pkg}-linux64'):
+        print(f"{pkg} failed to unzip . ")
+        return  
+    test(solver) 
+    return
+        
+def lp_test(solver):
+    import pyomo.environ as pyo
+    model = pyo.ConcreteModel()
+    model.x = pyo.Var()
+    model.c = pyo.Constraint(expr=model.x >= 0)
+    model.obj = pyo.Objective(expr=model.x)
+    try:
+        pyo.SolverFactory(solver).solve(model)
+    except:
+        print(f"{solver} failed test . ")
+    
+def ip_test(solver):
+    import pyomo.environ as pyo
+    model = pyo.ConcreteModel()
+    model.x = pyo.Var(domain=pyo.Integers)
+    model.c = pyo.Constraint(expr=model.x >= 0)
+    model.obj = pyo.Objective(expr=model.x)
+    try:
+        pyo.SolverFactory(solver).solve(model)
+    except:
+        print(f"{solver} failed test . ")
 
-def test_solver(solver):
+def test_solver(solver, test=):
         import pyomo.environ as pyo
         model = pyo.ConcreteModel()
         model.x = pyo.Var()
@@ -48,23 +72,23 @@ def test_solver(solver):
 
 async def install_pyomo():
     print("installing pyomo . ", end="")
-    returncode = await run("pip3 install -q pyomo")
-    if returncode:
+    if await run("pip3 install -q pyomo"):
         print("pyomo failed to install")
-    else:
-        print("pyomo installed")
-        print("installing solvers . ", end="")
-        await apt("glpk-utils", "glpk"),
-        await asyncio.gather(
-            pip("gurobipy", "gurobi_direct"),
-            #pip("xpress", "xpress"),
-            #pip("cplex", "cplex"),
-            apt("coinor-cbc", "cbc"),
-            ampl("ipopt", "ipopt"),
-            ampl("bonmin", "bonmin"),
-            ampl("couenne", "couenne"),
-            ampl("gecode", "gecode"),
-            #ampl("jacop", "jacop")
+        return
+    print("pyomo installed")
+    print("installing solvers . ", end="")
+    await apt_install("glpk-utils", "glpk"),
+    await asyncio.gather(
+        pip_install("gurobipy", "gurobi_direct"),
+        apt_install("coinor-cbc", "cbc"),
+        ampl_install("ipopt", "ipopt"),
+        ampl_install("bonmin", "bonmin"),
+        ampl_install("couenne", "couenne"),
+        ampl_install("gecode", "gecode", ip_test),
+            
+        #pip_install("xpress", "xpress"),
+        #pip_install("cplex", "cplex"),
+        #ampl_install("jacop", "jacop")
         )
     print("installation complete")
 
