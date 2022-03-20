1fd3dbc408bcb5aeb65e1a960afb5b20cd9f3dbf
Jeffrey Kantor <kantor.1@nd.edu>
2022-03-20 13:12:07 -0400

    rebuild book

--- _build/jupyter_execute/notebooks/02/fit_absolute.py
+++ _build/jupyter_execute/notebooks/02/fit_absolute.py
@@ -1,46 +1,41 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Example of minimizing a sum of absolute values
+# # Minimizing a sum of absolute values
 # 
 # We use linear regression minimizing the absolute deviations. 
 # 
 # We follow closely this [paper](https://www.jstor.org/stable/1402501).
-# 
-# Let us start by adding `pyomo` and `glpk` to our `colab` session.
 
-# In[ ]:
+# In[44]:
 
 
-get_ipython().system('pip install -q pyomo')
-
-
-# In[ ]:
-
-
-# we add glpk now
-get_ipython().system('sudo apt install libglpk-dev python3.8-dev libgmp3-dev')
-get_ipython().system('apt-get install -y -qq glpk-utils')
+# Install Pyomo and solvers for Google Colab
+import sys
+if "google.colab" in sys.modules:
+    get_ipython().system('wget -N -q https://raw.githubusercontent.com/jckantor/MO-book/main/tools/install_on_colab.py ')
+    get_ipython().run_line_magic('run', 'install_on_colab.py')
 
 
 # # Generate data
 
-# In[ ]:
+# In[34]:
 
-
-n_features = 1 # 1 may be visualized, > 1 works fine but no drawing
-n_samples  = 1000
-noise      = 30
 
 from sklearn.datasets import make_regression
 import matplotlib.pyplot as plt
 import numpy as np
-np.random.seed(2020)
+
+n_features = 1
+n_samples = 1000
+noise = 30
+
 # generate regression dataset
+np.random.seed(2020)
 X, y = make_regression(n_samples=n_samples, n_features=n_features, noise=noise)
 
 if n_features == 1:
-    plt.scatter(X,y)
+    plt.scatter(X, y)
     plt.show()
 
 
@@ -48,65 +43,75 @@ if n_features == 1:
 # 
 # 
 
-# In[ ]:
+# In[35]:
 
 
 import pyomo.environ as pyo
 
 
-# In[ ]:
+# In[36]:
 
 
 m = pyo.ConcreteModel('min abs')
 
 
-# In[ ]:
+# In[37]:
 
 
-n,k = X.shape
+n, k = X.shape
 
 
-# In[ ]:
+# In[38]:
 
 
-m.I = range(n)
-m.J = range(k)
+m.I = pyo.RangeSet(0, n-1)
+m.J = pyo.RangeSet(0, k-1)
 
 
-# In[ ]:
+# In[39]:
 
 
-m.ep = pyo.Var(m.I,within=pyo.NonNegativeReals)
-m.em = pyo.Var(m.I,within=pyo.NonNegativeReals)
-m.m  = pyo.Var(m.J)
-m.b  = pyo.Var()
+m.ep = pyo.Var(m.I, domain=pyo.NonNegativeReals)
+m.em = pyo.Var(m.I, domain=pyo.NonNegativeReals)
+m.m = pyo.Var(m.J)
+m.b = pyo.Var()
 
 
-# In[ ]:
+# In[40]:
 
 
-m.fit = pyo.Constraint( m.I, rule = lambda m, i : m.b + m.ep[i] - m.em[i] + sum(X[i][j]*m.m[j] for j in m.J) == y[i] )
+@m.Constraint(m.I)
+def fit(m, i):
+    return y[i] == m.b + m.ep[i] - m.em[i] + sum(X[i][j]*m.m[j] for j in m.J)
 
 
-# In[ ]:
+# In[41]:
 
 
-m.obj = pyo.Objective( expr = sum( m.ep[i] + m.em[i] for i in m.I ), sense = pyo.minimize )
+@m.Objective(sense=pyo.minimize)
+def obj(m):
+    return sum(m.ep[i] + m.em[i] for i in m.I)
 
 
-# In[ ]:
+# In[42]:
 
 
 get_ipython().run_line_magic('time', "pyo.SolverFactory('glpk').solve(m)")
 
 
-# In[ ]:
+# In[43]:
 
 
 if n_features ==  1:
-    plt.scatter(X,y)
-    plt.plot([ x[0] for x in X ], [ pyo.value(m.b) + pyo.value(m.m[0])*x[0] for x in X ], 'r' )
+    plt.scatter(X, y, alpha=0.5)
+    plt.plot([x[0] for x in X], [pyo.value(m.b) + pyo.value(m.m[0])*x[0] for x in X ], 'r')
     plt.show()
 else:
     print( pyo.value(m.b),[pyo.value(m) for m in m.m.values() ] )
+
+
+# In[ ]:
+
+
+
 
