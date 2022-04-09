f32e56bf2c079d0f144a2fc64452700186266873
Jeffrey Kantor <kantor.1@nd.edu>
2022-04-09 12:45:27 -0400

    rebuild book

--- _build/jupyter_execute/notebooks/02/making-the-best-of-the-worst.py
+++ _build/jupyter_execute/notebooks/02/making-the-best-of-the-worst.py
@@ -3,7 +3,7 @@
 
 # # Making the Best of the Worst
 
-# In[112]:
+# In[1]:
 
 
 # Install Pyomo and solvers for Google Colab
@@ -56,20 +56,20 @@ if "google.colab" in sys.modules:
 
 # ## Data
 
-# In[115]:
+# In[99]:
 
 
 import pandas as pd
 
-BIM_profit_scenarios = pd.DataFrame([
+BIM_scenarios = pd.DataFrame([
     [12, 9], 
     [11, 10], 
     [8, 11]], 
     columns=["product 1", "product 2"])
 
-BIM_profit_scenarios.index.name = "scenarios"
-
-display(BIM_profit_scenarios)
+BIM_scenarios.index.name = "scenarios"
+print("\nProfit scenarios")
+display(BIM_scenarios)
 
 BIM_resources = pd.DataFrame([
     ["silicon", 1000, 1, 0],
@@ -79,39 +79,45 @@ BIM_resources = pd.DataFrame([
     columns = ["resource", "available", "product 1", "product 2"])
 BIM_resources = BIM_resources.set_index("resource")
     
+print("\nAvailable resources and resource requirements")
 display(BIM_resources)
     
 
 
 # ## Pyomo Model
+# 
+# An implementation of the maximum worst-case profit model.
 
-# In[119]:
+# In[159]:
 
 
 import pyomo.environ as pyo
 
-def maximin(profit_scenarios, resources):
+def maxmin(scenarios, resources):
     
     model    = pyo.ConcreteModel('BIM')
     
+    products = resources.columns.tolist()
+    products.remove('available')
+    
     model.I = pyo.Set(initialize=resources.index)
-    model.J = pyo.Set(initialize=profit_scenarios.columns)
-    model.S = pyo.Set(initialize=profit_scenarios.index)
+    model.J = pyo.Set(initialize=products)
+    model.S = pyo.Set(initialize=scenarios.index)
     
     model.a = pyo.Param(model.I, model.J, rule = lambda model, i, j: resources.loc[i, j])
     model.b = pyo.Param(model.I, rule = lambda model, i: resources.loc[i, "available"])
-    model.c = pyo.Param(model.S, model.J, rule = lambda model, s, j: profit_scenarios.loc[s, j])
+    model.c = pyo.Param(model.S, model.J, rule = lambda model, s, j: scenarios.loc[s, j])
     
     model.x = pyo.Var(model.J, domain=pyo.NonNegativeReals)
     model.z  = pyo.Var() 
     
     @model.Objective(sense=pyo.maximize)
-    def maxmin_profit(model):
+    def profit(model):
         return model.z
     
     @model.Constraint(model.S)
-    def scenario_profit(model, s):
-        return model.z <= sum(model.c[s, j] * model.x[j] for j in model.J)
+    def scenario_profit(m, s):
+        return m.z <= sum(model.c[s, j] * model.x[j] for j in model.J)
         
     @model.Constraint(model.I)
     def resource_limits(model, i):
@@ -119,14 +125,86 @@ def maximin(profit_scenarios, resources):
 
     return model
 
-BIM = BIM_maxmin(BIM_profit_scenarios, BIM_resources)
+BIM = maxmin(BIM_scenarios, BIM_resources)
 pyo.SolverFactory('glpk').solve(BIM)
 
-results = pd.DataFrame([BIM.x[j]() for j in BIM.J], index=BIM.J, columns=["production"])
-worst_case_profit = BIM.z()
+worst_case_plan = pd.Series({j: BIM.x[j]() for j in BIM.J})
+worst_case_profit = BIM.profit()
 
-print("worst case profit = ", worst_case_profit)
-display(results)
+print("\nworst case profit = ", worst_case_profit)
+print(f"\nworst case production plan:\n")
+display(worst_case_plan)
+
+
+# ## Is maximizing the worst case a good idea?
+# 
+# Maximizing the worst case among all scenarios may be a pessimistic planning outlook. It may be worth investigating alternative planning outlooks The first step is to create a model to optimize a single scenario. Without repeating the mathematical description, the following Pyomo model is simply the `maxmin` model adapted to a single scenario.
+
+# In[160]:
+
+
+def max_profit(scenario, resources):
+    
+    model    = pyo.ConcreteModel('BIM')
+    
+    products = resources.columns.tolist()
+    products.remove('available')
+    
+    model.I = pyo.Set(initialize=resources.index)
+    model.J = pyo.Set(initialize=products)
+    
+    model.a = pyo.Param(model.I, model.J, rule = lambda model, i, j: resources.loc[i, j])
+    model.b = pyo.Param(model.I, rule = lambda model, i: resources.loc[i, "available"])
+    model.c = pyo.Param(model.J, rule = lambda model, j: scenario[j])
+    
+    model.x = pyo.Var(model.J, domain=pyo.NonNegativeReals)
+    model.z  = pyo.Var() 
+    
+    @model.Objective(sense=pyo.maximize)
+    def profit(m, s):
+        return sum(model.c[j] * model.x[j] for j in model.J)
+        
+    @model.Constraint(model.I)
+    def resource_limits(model, i):
+        return sum(model.a[i, j] * model.x[j] for j in model.J) <= model.b[i]
+
+    return model
+
+
+# ## Optimizing for the mean scenario
+# 
+# The next cell computes the optimal plan for the mean scenario.
+
+# In[166]:
+
+
+# create mean scenario
+
+mean_case = max_profit(BIM_scenarios.mean(), BIM_resources)
+pyo.SolverFactory('glpk').solve(mean_case)
+
+mean_case_profit = mean_case.profit()
+mean_case_plan = pd.Series({j: m.x[j]() for j in m.J})
+
+print("\nmean case profit", mean_case_profit)
+print("\nmean case production plan\n")
+print(mean_case_plan)
+
+
+# The expected profit under the mean scenario if 17,833, which is 333 greater than maximizing the worst case. Also note the change in production plan. Would plan should be preferred ... one that produces no less than 17,500 under all scenarios, or one that produces 17,833 when averaged over all scenarios?
+
+# In[178]:
+
+
+mean_outcomes = BIM_scenarios.dot(mean_case_plan)
+worst_case_outcomes = BIM_scenarios.dot(worst_case_plan)
+mean_outcomes.plot(kind="bar")
+
+
+# In[179]:
+
+
+worst_case_outcomes.plot(kind="bar")
 
 
 # In[ ]:
