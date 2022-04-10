7e36c4b8800f291f3acfa2e7af2c98ea34005d83
Jeffrey Kantor <kantor.1@nd.edu>
2022-04-10 09:36:10 -0400

    Update mobook.py

--- tools/mobook.py
+++ tools/mobook.py
@@ -2,19 +2,26 @@ import sys
 import subprocess
 import matplotlib as mpl
 
-def on_colab():
-    return "google.colab" in sys.modules
 
 def mplstyle():
     mpl.rcParams['font.family'] = 'STIXgeneral'
     mpl.rcParams['mathtext.fontset'] = 'stix'
-    mpl.rcParams['axes.titlesize'] = 30
+    mpl.rcParams['axes.titlesize'] = 18
     mpl.rcParams['pdf.fonttype'] = 42
 
 def pip_install(package):
     print(f"installing {package}")
     subprocess.check_call([sys.executable, "-m", "pip", "install", "-q", package])
     
+def apt_install(package):
+    print(f"installing {package} via apt")
+    subprocess.check_call([sys.executable, "-m", "apt-get", "install", "-y", "-qq", package])
+   
+# default installations for Google Colab
+def on_colab():
+    return "google.colab" in sys.modules
+
 if on_colab():
     pip_install("pyomo")
+    apt_install("glpk-utils")
     
