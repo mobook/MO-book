e14bbe01e93a222eaa92637ff01baea320144c99
Jeffrey Kantor <kantor.1@nd.edu>
2022-04-10 08:30:32 -0400

    update mobook.py

--- tools/mobook.py
+++ tools/mobook.py
@@ -1,12 +1,15 @@
 import sys
+import subprocess
 
 def on_colab():
     return "google.colab" in sys.modules
 
 def mplstyle():
     pass
+
+def pip_install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-q", package])
     
 if on_colab():
-    print("Hi from Colab")
-
-
+    pip_install("pyomo")
+    
