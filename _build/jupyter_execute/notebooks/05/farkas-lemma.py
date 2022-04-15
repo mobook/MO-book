b1134f593ceb1278bbd07803eb3c99ee445175b9
Jeffrey Kantor <kantor.1@nd.edu>
2022-04-15 07:02:06 -0400

    updatess

---
+++ _build/jupyter_execute/notebooks/05/farkas-lemma.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Minimum Risk-Free Rate of Return
+# 
+# 
+# Application of Farka's lemma and convex duality. - Ar
+# 
+# 
+# ## Example
+# 
+# * Convert 2A -> P (commodity goods). There's a cost to operate
+# * Owner holds option to produce quantity q of P at time T
+# * Multiple price scenarios for A and P
+# * There's a risk free alternative investment with payoff Rf
+# 
+# Determine -- difference between seller and buyer
+# 
+# 
