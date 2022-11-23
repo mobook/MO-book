0dace6b39208712ca026a203f4d3177b9def530d
Jeffrey Kantor <kantor.1@nd.edu>
2022-11-23 14:03:44 -0500

    rebuild book

--- _build/jupyter_execute/notebooks/07/fleet-assignment.py
+++ _build/jupyter_execute/notebooks/07/fleet-assignment.py
@@ -3,7 +3,7 @@
 
 # # Fleet assignment problem
 
-# In[1]:
+# In[481]:
 
 
 # install Pyomo and solvers
@@ -24,7 +24,9 @@ helper.install_cbc()
 # 
 # The task of the airline operations team is to determine the minimum number of airplanes needed to operate the given list of flights. This problem is known as the **fleet assignment problem** or **aircraft rotation problem**.
 
-# In[324]:
+# ### Generate Flight Data
+
+# In[546]:
 
 
 import numpy as np
@@ -47,77 +49,93 @@ def generate_flights(N_flights=30, min_duration=1, max_duration=4, max_departure
 FlightData = pd.DataFrame([[flight, departure, arrival] for flight, departure, arrival in generate_flights()])
 FlightData.columns = ["Flight", "Departure", "Arrival"]
 FlightData.set_index("Flight", inplace=True)
+FlightData.head()
 
 
-# ## Visualize Flight Data
+# ### Visualize Flight Data
 
-# In[335]:
+# In[682]:
 
 
 # visualize flight data
 def draw_flights(FlightData):
     bar_style = {'alpha':1.0, 'lw':10, 'solid_capstyle':'butt'}
     text_style = {'fontsize': 7, 'color':'white', 'weight':'bold', 'ha':'center', 'va':'center'}
-    fig = plt.figure(figsize=(12, 8))
+    fig = plt.figure(figsize=(12, 7))
     ax = fig.add_subplot(111, xlabel="Departure/Arrival Time", title="FlightData")
     ax.get_yaxis().set_visible(False)
     for flight, row in FlightData.iterrows():
         departure, arrival = row
         ax.plot([departure, arrival], [flight]*2, 'gray', **bar_style)
         ax.text((departure + arrival)/2, flight, flight, **text_style)
+    for hr in range(25):
+        ax.axvline(hr, alpha=0.1)
+    ax.set_xlim(-1, 25)
+    ax.set_xticks([4*i for i in range(7)])
     return ax
 
 draw_flights(FlightData)
 
 
-# ## Find All Feasible Flight Pairs
+# ## Pyomo Model
 
-# In[337]:
+# The fleet assignment problem can be formulated and solved as an MILP. The idea of the MILP formulation is to construct a feasible path in a directed graph where the flights are nodes with indices $\mathcal{F} = \left\{ 1, \ldots, F \right\}$. The set of arcs $\mathcal{A} \subseteq \mathcal{F} \times \mathcal{F}$ that can be used by each aircraft is then:
+# 
+# $$
+# \mathcal{A} = \{ a = (f_1, f_2): f_1 \text{ arrives at least 1h before the departure of } f_2, \ f_1, f_2 \in \mathcal{F} \}
+# $$
+# 
+# The following cell finds the set of arcs that can be used. These arcs are displayed in a graph of the flight data. Arcs corresponding to the minimum time between arrival and departure are highlighted.
 
+# In[683]:
+
+
+min_time = 1
 
 # create an iterator to produce pairs of flight numbers
 flight_pairs = list(filter(
-    lambda pair: FlightData.loc[pair[0], "Arrival"] < FlightData.loc[pair[1], "Departure"],
+    lambda pair: FlightData.loc[pair[0], "Arrival"] + min_time <= FlightData.loc[pair[1], "Departure"],
     [[i, j] for i in FlightData.index for j in FlightData.index if i != j]
 ))
-
-
-# ## Visualize Feasible Flight Pairs as a Directed Graph
-
-# In[338]:
-
 
 ax = draw_flights(FlightData)
 for flight1, flight2 in flight_pairs:
     arr = FlightData.loc[flight1, "Arrival"]
     dep = FlightData.loc[flight2, "Departure"]
-    c = 'r' if dep - arr <= 1 else 'k'
+    c = 'r' if dep - arr <= min_time else 'g'
     ax.plot([arr, dep], [flight1, flight2], color=c, lw=1, alpha=0.4)
 
 
-# ## Directed Graph
-# 
-# Networkx includes useful tools for the modeling and analysis of directed graphs. For this application, we use Networkx to compute the input edges and output edges for each flight node. These computations are straightforward and could easily be done with a few lines of Python, but Networkx makes the overall model more expressive and easy to read.
+# For each node $f\in\mathcal{F}$ we define a set of input nodes $\mathcal{I}_f = \{f_1: (f_1, f)\in\mathcal{A}\}$  and a set of output nodes $\mathcal{O}_f = \{f_1: (f, f_1)\in\mathcal{A} \}$. For this application, we use the Python ``set`` object to scan the feasible flight pairs and find the inputs and outputs nodes for each flight node. 
 
-# In[330]:
+# In[684]:
 
 
-# create DiGraph
+in_nodes = {flight: set() for flight in FlightData.index}
+out_nodes = {flight: set() for flight in FlightData.index}
 
-DG = nx.DiGraph()
-
-# display all nodes
-for flight in FlightData.index:
-    DG.add_node(flight)
-
-# display all edges
 for flight1, flight2 in flight_pairs:
-    DG.add_edge(flight1, flight2)
+    in_nodes[flight2].add(flight1)
+    out_nodes[flight1].add(flight2)
 
 
-# ## Pyomo Model
+# The decision variables are:
+# 
+# * $x_{f_1, f_2}\in\{0,1\}$ for all $(f_1, f_2) \in\mathcal{A}$ where $x_{f_1, f_2} = 1$ indicates that an aircraft used for arriving flight $f_1$ will used for departing flight $f_2$.
+# 
+# * $p_f\in\{0,1\}$ for all $f\in\mathcal{F}$ where $p_f = 1$ indicates a previously unassigned aircraft will be use for departing flight $f$.
+# 
+# * $q_f\in\{0,1\}$ for all $f\in\mathcal{F}$ where $q_f = 1$ indicates an aircraft will be unassigned after arrival of flight $f$.
+# 
+# The binary variables $p_f$ and $q_f$ correspond to arcs that link nodes to the sources and sinks of aircraft needed to complete all of the flights $f\in\mathcal{F}$. The objective is to minimize the number of required aircraft subject to the constraints that exactly one aircraft is a assigned to and released from each node.
+# 
+# $$\begin{align}
+# \min\limits_{x_a, p_f, q_f}\quad & \sum_{f\in\mathcal{F}} p_f \\
+# \text{s.t.}\quad & p_f + \sum_{f_1\in\mathcal{I}_f} x_{f_1, f} = 1 & \forall f\in\mathcal{F} \\
+# & q_f + \sum_{f_1\in\mathcal{O}_f} x_{f, f_1} = 1 & \forall f\in\mathcal{F}
+# \end{align}$$
 
-# In[339]:
+# In[685]:
 
 
 m = pyo.ConcreteModel("Fleet Assignment")
@@ -126,71 +144,81 @@ m.FLIGHTS = pyo.Set(initialize=FlightData.index)
 m.PAIRS = pyo.Set(initialize=flight_pairs)
 
 m.x = pyo.Var(m.PAIRS, domain=pyo.Binary)
-m.source = pyo.Var(m.FLIGHTS, domain=pyo.Binary)
-m.sink = pyo.Var(m.FLIGHTS, domain=pyo.Binary)
-
-@m.Expression(m.FLIGHTS)
-def sum_in_edges(m, flight):
-    return m.source[flight] + sum(m.x[in_flight, flight] for in_flight, _ in DG.in_edges(flight))
-
-@m.Expression(m.FLIGHTS)
-def sum_out_edges(m, flight):
-    return m.sink[flight] + sum(m.x[flight, out_flight] for _, out_flight in DG.out_edges(flight))
+m.p = pyo.Var(m.FLIGHTS, domain=pyo.Binary)
+m.q = pyo.Var(m.FLIGHTS, domain=pyo.Binary)
 
 @m.Constraint(m.FLIGHTS)
-def one_airplane_for_each_flight(m, flight):
-    return m.sum_in_edges[flight] == 1
+def sources(m, flight):
+    return m.p[flight] + sum(m.x[_, flight] for _ in in_nodes[flight]) == 1
 
 @m.Constraint(m.FLIGHTS)
-def balance(m, flight): 
-    return m.sum_in_edges[flight] == m.sum_out_edges[flight]
+def sinks(m, flight): 
+    return m.q[flight] + sum(m.x[flight, _] for _ in out_nodes[flight]) == 1
 
 @m.Objective(sense=pyo.minimize)
 def minimize_airplanes(m):
-    return pyo.summation(m.source)
+    return pyo.summation(m.p)
 
 pyo.SolverFactory("cbc").solve(m)
 
 print(f"Minimum airplanes required = {m.minimize_airplanes()}")
+
+
+# We visualize the solution by drawing arcs where $x_{f_1, f_2} = 1$ and where $p_f = 1$ and $q_f = 1$. These arcs draw feasible paths through the graph corresponding to the assignment of one aircraft to service one or more flights.
+
+# In[686]:
+
 
 ax = draw_flights(FlightData)
 for flight1, flight2 in flight_pairs:
     if m.x[flight1, flight2]() == 1:
         arr = FlightData.loc[flight1, "Arrival"]
         dep = FlightData.loc[flight2, "Departure"]
-        c = 'r' if dep - arr <= 1 else 'k'
+        c = 'r' if dep - arr <= min_time else 'g'
         ax.plot([arr, dep], [flight1, flight2], color=c, lw=4, alpha=0.4)
 
-
-# ## Allocate Aircraft
-
-# In[435]:
-
-
-aircraft = 0
-at_risk = 0
 for flight in FlightData.index:
-    if m.source[flight]() == 1:
-        aircraft += 1
-        print(f"\nAircraft {aircraft:2d}")
-        print(f"    Flight {flight:2d}: depart {FlightData.loc[flight, 'Departure']:2d}", end="")
-        print(f" --> arrive {FlightData.loc[flight, 'Arrival']:2d}")
-        f = flight
-        while m.sink[f]() != 1:
-            for _, g in DG.out_edges(f):
+    if m.p[flight]() == 1:
+        dep = FlightData.loc[flight, "Departure"]
+        ax.plot([-1, dep], [flight]*2, 'g', lw=4, alpha=0.4)
+    if m.q[flight]() == 1:
+        arr = FlightData.loc[flight, "Arrival"]
+        ax.plot([arr, 25], [flight]*2, 'g', lw=4, alpha=0.4)
+
+
+# ## Allocate Aircraft to Flights
+
+# In[671]:
+
+
+n = 0
+FlightSchedule = FlightData.copy()
+
+for f in FlightData.index:
+    if m.p[f]() == 1:
+        n += 1
+        FlightSchedule.loc[f, "Aircraft"] = f"{n:2d}"
+        while m.q[f]() != 1:
+            for g in out_nodes[f]:
                 if m.x[f, g]() == 1:
                     break
-            dt = FlightData.loc[g, "Departure"] - FlightData.loc[f, "Arrival"]
-            if dt <= 1:
-                print(f"  **", end="")
-                at_risk += 1
-            else:
-                print(f"    ", end="")
-            print(f"Flight {g:2d}: depart {FlightData.loc[g, 'Departure']:2d}", end="")
-            print(f" --> arrive {FlightData.loc[g, 'Arrival']:2d}")
+            FlightSchedule.loc[g, "Aircraft"] = f"{n:2d}"
             f = g
-            
-print(f"\n**{at_risk} flights at risk of delay")
+
+FlightSchedule = FlightSchedule[["Aircraft", "Departure", "Arrival"]]
+FlightSchedule = FlightSchedule.sort_values(["Aircraft", "Departure"])
+FlightSchedule["Flight"] = FlightSchedule.index
+ind = pd.MultiIndex.from_frame(FlightSchedule[["Aircraft", "Flight"]])
+FlightSchedule.index = ind
+FlightSchedule = FlightSchedule[["Departure", "Arrival"]]
+FlightSchedule
+
+
+# In[655]:
+
+
+FS = FlightSchedule.sort_values(["Aircraft", "Departure"])
+ind = pd.MultiIndex(FS[["Aircraft"]
 
 
 # ## Reformulation the fleet assignment as a network problem
@@ -228,49 +256,7 @@ print(f"\n**{at_risk} flights at risk of delay")
 # - \eqref{eq:71f} is a symmetry-breaking constraint, which makes sure that we only use airplanes that come first in the aircraft set.
 # 
 
-# In[316]:
-
-
-import networkx as nx
-
-DG = nx.DiGraph()
-
-# make sure all nodes are displayed, even if unconnected
-for flight in FlightData.index:
-    DG.add_node(flight)
-
-
-flight_pairs = filter(
-    lambda pair: FlightData.loc[pair[0], "Arrival"] < FlightData.loc[pair[1], "Departure"],
-    [[i, j] for i in FlightData.index for j in FlightData.index if i != j]
-)
-
-# ordered pairs
-for flight1, flight2 in flight_pairs:
-    DG.add_edge(flight1, flight2)
-    if FlightData.loc[flight1, "Arrival"] + 1 == FlightData.loc[flight2, "Departure"]:
-        DG[flight1][flight2]["color"] = "r"
-    else:
-        DG[flight1][flight2]["color"] = "k"
-        
-# add dummy START
-DG.add_node("START")
-for flight in FlightData.index:
-    DG.add_edge("START", flight)
-    DG["START"][flight]["color"] = "b"
-    
-# add dummy FINISH
-DG.add_node("FINISH")
-for flight in FlightData.index:
-    DG.add_edge(flight, "FINISH")
-    DG[flight]["FINISH"]["color"] = "b"
-    
-fig = plt.figure(figsize=(8, 8))
-nx.draw(DG, nx.circular_layout(DG), with_labels=True, font_size=8, arrowsize=10,
-        node_color="gold", edge_color=[DG.edges[u, v]["color"] for u, v in DG.edges])
-
-
-# In[4]:
+# In[443]:
 
 
 def flight_network(FlightData):
@@ -422,7 +408,7 @@ visualize(m, FlightData, "optimalschedule", seed)
 # We will now to keep the maximum number of flights at the optimal level, but try to minimize their riskiness. To do so, we define a slightly different MILP that takes the minimum number of planes `nplanes` in input and has the total number of risky pairs as objective function.
 # 
 
-# In[5]:
+# In[ ]:
 
 
 def fleet_assignment_minimum_risk(FlightData, flight_pairs, pair_riskiness, start_pairs, end_pairs, nplanes):
@@ -496,4 +482,10 @@ m2 = fleet_assignment_minimum_risk(FlightData, flight_pairs, pair_riskiness, sta
 print(f'The optimal solution for this instance of the fleet assignment uses a total of {pyo.value(pyo.quicksum(m2.y[aircraft] for aircraft in m2.A)):.0f} planes.\n')
 print(f'The number of risky pairs (back-to-back flights operated by the same airplane with exactly 1h between them) in this new solution is equal to {pyo.value(m2.riskobjective):.0f}.\n')
 visualize(m2, FlightData, "optimalschedule_minrisk", seed)
+
+
+# In[ ]:
+
+
+
 
