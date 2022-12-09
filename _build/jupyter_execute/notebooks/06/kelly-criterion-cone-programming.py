e86581a2b1791c962c0d2f7d065401f6a596fb0d
Jeffrey Kantor <kantor.1@nd.edu>
2022-12-09 10:55:22 -0500

    rebuild book

--- _build/jupyter_execute/notebooks/06/kelly-criterion-cone-programming.py
+++ _build/jupyter_execute/notebooks/06/kelly-criterion-cone-programming.py
@@ -13,6 +13,22 @@
 # 
 # Following the analysis of Cajas (2021), this notebook presents a version of the the problem analysis demonstrating the applcation of exponential cone programming. 
 
+# In[1]:
+
+
+# install Pyomo and solvers
+import requests
+import types
+
+url = "https://raw.githubusercontent.com/mobook/MO-book/main/python/helper.py"
+helper = types.ModuleType("helper")
+exec(requests.get(url).content, helper.__dict__)
+
+helper.install_pyomo()
+helper.install_mosek()
+helper.install_gurobi()
+
+
 # ## Problem
 # 
 # The classical presentation of the Kelly Criterion is to consider a betting proposition offering odds of $b$ for an outcome with propability $p$. In that case a bet pays off $b+1$ unit of money for every unit of money wagered. If we let $x$ represent the fraction of wealth that is wagered, then the gross returns are given by 
@@ -70,6 +86,66 @@
 # 
 
 # ## Example 1: Proposition Betting
+# 
+# $$
+# \begin{align}
+# \max_w&\quad p q_1 + (1-p) q_2 \\
+# \text{s.t}\quad&\\
+# & (q_1, 1, 1 + bw) \in K_{exp} \\
+# & (q_2, 1, 1 - w) \in K_{exp} \\
+# & w \geq 0
+# \end{align}
+# $$
+# 
+# **Programming Note:** The Pyomo Kernel Library includes exponential cone constraints, but reorders the arguments. As presented above, $K_{exp} = \{(x,y,r)\}$ refers to the convex cone described by
+# 
+# $$y \exp(\frac{x}{y}) \leq r$$
+# 
+# where $y, r \geq 0$. The Pyomo Kernel Library defines the same cone as 
+# 
+# $$x_1 \exp(\frac{x_2}{x_1}) \leq r$$
+# 
+# where $x_1, r \geq 0$. The concordance is then
+# 
+# $$
+# \begin{align}
+# x & \sim x_2 \\
+# y & \sim x_1 \\
+# r & \sim r
+# \end{align}
+# $$
+
+# In[48]:
+
+
+b = 1
+p = 0.51
+
+
+# In[49]:
+
+
+import pyomo.kernel as pmo
+
+m = pmo.block()
+
+# decision variables
+m.q1 = pmo.variable()
+m.q2 = pmo.variable()
+m.w = pmo.variable(lb=0)
+
+# objective
+m.lnR = pmo.objective(p*m.q1 + (1-p)*m.q2, sense=pmo.maximize)
+
+# conic constraints
+m.t1 = pmo.conic.primal_exponential.as_domain(1 + b*m.w, 1, m.q1)
+m.t2 = pmo.conic.primal_exponential.as_domain(1 - m.w, 1, m.q2)
+
+# solve with 'mosek_direct' or 'gurobi_direct'
+pmo.SolverFactory('mosek_direct').solve(m)
+
+m.lnR()
+
 
 # ## Example 2: Luenberger's Betting Wheel
 # 
@@ -77,9 +153,42 @@
 # * https://www.math.nyu.edu/~kohn/undergrad.finance/2003/hw2.pdf
 # 
 
+# In[ ]:
+
+
+import pyomo.kernel as pmo
+
+m = pmo.block()
+
+# decision variables
+m.q1 = pmo.variable()
+m.q2 = pmo.variable()
+m.q3 = pmo.variable()
+
+m.w = pmo.variable(lb=0)
+m.w = pmo.variable(lb=0)
+m.w = pmo.variable(lb=0)
+
+# objective
+m.lnR = pmo.objective(p*m.q1 + (1-p)*m.q2, sense=pmo.maximize)
+
+# conic constraints
+m.t1 = pmo.conic.primal_exponential.as_domain(1 + b*m.w, 1, m.q1)
+m.t2 = pmo.conic.primal_exponential.as_domain(1 - m.w, 1, m.q2)
+
+# solve with 'mosek_direct' or 'gurobi_direct'
+pmo.SolverFactory('mosek_direct').solve(m)
+
+m.lnR()
+
+
 # ## Example 3: Volatility Pumping with Index Funds
+# 
+# Following Luenberger Chapter 6, Thorp, Cajas.
 
 # ## Example 4: Portfolio Design with Drawdown Constraints
+# 
+# Following Busseti, Boyd (2016)
 
 # ## Relationship with Relative Entropy Optimization
 # 
@@ -89,6 +198,14 @@
 
 # ## Bibliographic Notes
 # 
+# The Kelly Criterion has been included in many tutorial introductions to finance and probability, and the subject of popular accounts.
+# 
+# > Poundstone, W. (2010). Fortune's formula: The untold story of the scientific betting system that beat the casinos and Wall Street. Hill and Wang. 
+# 
+# >  https://en.wikipedia.org/wiki/Kelly_criterion
+# 
+# The utility of conic programming to solve problems involving log growth is more recent. Here are some representative papers.
+# 
 # > Cajas, D. (2021). Kelly Portfolio Optimization: A Disciplined Convex Programming Framework. Available at SSRN 3833617. https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3833617
 # 
 # > Busseti, E., Ryu, E. K., & Boyd, S. (2016). Risk-constrained Kelly gambling. The Journal of Investing, 25(3), 118-134. https://arxiv.org/pdf/1603.06183.pdf
@@ -96,4 +213,10 @@
 # 
 # > Fu, A., Narasimhan, B., & Boyd, S. (2017). CVXR: An R package for disciplined convex optimization. arXiv preprint arXiv:1711.07582. https://arxiv.org/abs/1711.07582
 # 
-# > Poundstone, W. (2010). Fortune's formula: The untold story of the scientific betting system that beat the casinos and Wall Street. Hill and Wang. 
+# 
+
+# In[ ]:
+
+
+
+
